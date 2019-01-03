9bfa31495d6d123c324edab33bcb5e8d3a34c3a8
UkiAbe <unlimited.blade.works22@gmail.com>
2019-01-03 15:35:11 +0900

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,6 +4,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
+    @non_activation = users(:lana)
   end
 
   test "index as admin include pagination and delete link" do
@@ -12,9 +13,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
     User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: "delete"
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: "delete"
+        end
       end
     end
     assert_difference "User.count", -1 do
@@ -26,5 +29,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index as non-activation" do
+    log_in_as(@admin)
+    get users_path
+    assert_select 'a', text: @non_activation.name, count: 0
+    get user_path(@non_activation)
+    follow_redirect!
+    assert_template "static_pages/home"
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
@@ -16,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'ul'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference "User.count", 1 do
       post signup_path, params: { user: { name: "exampleUser",
@@ -24,6 +29,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                             password: "password",
                             password_confirmation: "password"}}
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    # 有効化トークンが違う場合
+    get edit_account_activation_url('wrong', email: user.email)
+    assert_not is_logged_in?
+    # トークンは正しいが、メールアドレスが違う場合
+    get edit_account_activation_url(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # トークンもアドレスも正しい場合
+    get edit_account_activation_url(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template "users/show"
     assert is_logged_in?
